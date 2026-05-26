7d31725d5e3ecdbc0a9b5ed8cb6623d190ef88a3
Simon Lauper <simon.lauper@ceff.ch>
2026-05-26 11:43:42 +0200

    implementé pion movement

---
+++ models/pawn.js
@@ -0,0 +1,11 @@
+import Piece from "./piece.js"
+
+class Pawn extends Piece {
+    move(plateau) {
+        if (isWhite){
+            plateau[y-1][x].piece == null;
+        } else {
+
+        }
+    }
+}
---
+++ models/piece.js
@@ -0,0 +1,7 @@
+class Piece {
+    constructor(x, y, isWhite = true) {
+        this.x = x;
+        this.y = y;
+        this.white = isWhite;
+    }
+}
